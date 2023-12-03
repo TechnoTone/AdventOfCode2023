68d16d1ee517c81c21b625d0669a313ea34cb28a
Tony Hunt <thunt@findmypast.com>
2023-12-03 16:14:46 +0000

    Remove unnecessary Jest Console "hack" from template file

--- JS/.fttemplates/AOC Day/day[FTName].test.js
+++ JS/.fttemplates/AOC Day/day[FTName].test.js
@@ -20,13 +20,3 @@ test.skip("Part 2", () => {
   const input = new Input([FTName]).fromLines().get();
   expect(part2(input)).toBe(0);
 });
-
-const jestConsole = console;
-
-beforeEach(() => {
-  global.console = require("console");
-});
-
-afterEach(() => {
-  global.console = jestConsole;
-});
