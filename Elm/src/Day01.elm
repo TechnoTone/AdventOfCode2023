9b1573f4c445c9b002396b0f313a919b570a16db
Tony Hunt <thunt@findmypast.com>
2023-12-05 16:59:30 +0000

    Elm Day 1

---
+++ Elm/src/Day01.elm
@@ -0,0 +1,110 @@
+module Day01 exposing
+    ( digitDictionary
+    , digitWords
+    , getFirstDigitWord
+    , getLastDigitWord
+    , part1
+    , part2
+    , toCalibrationValue
+    , toCalibrationValueIncludingWords
+    )
+
+import Dict exposing (Dict)
+
+
+part1 : String -> Int
+part1 input =
+    input
+        |> String.lines
+        |> List.filterMap toCalibrationValue
+        |> List.sum
+
+
+toCalibrationValue : String -> Maybe Int
+toCalibrationValue input =
+    let
+        digits : String
+        digits =
+            input
+                |> String.filter Char.isDigit
+    in
+    String.left 1 digits
+        ++ String.right 1 digits
+        |> String.toInt
+
+
+part2 input =
+    input
+        |> String.lines
+        |> List.filterMap toCalibrationValueIncludingWords
+        |> List.sum
+
+
+toCalibrationValueIncludingWords : String -> Maybe Int
+toCalibrationValueIncludingWords input =
+    case
+        [ getFirstDigitWord input
+        , getLastDigitWord input
+        ]
+            |> List.filterMap (\digit -> Dict.get digit digitDictionary)
+    of
+        [ a, b ] ->
+            Just (a * 10 + b)
+
+        _ ->
+            Nothing
+
+
+getFirstDigitWord : String -> String
+getFirstDigitWord input =
+    if input == "" then
+        ""
+
+    else
+        digitWords
+            |> List.filter (\word -> String.startsWith word input)
+            |> List.head
+            |> Maybe.withDefault
+                (getFirstDigitWord (String.right (String.length input - 1) input))
+
+
+getLastDigitWord : String -> String
+getLastDigitWord input =
+    if input == "" then
+        ""
+
+    else
+        digitWords
+            |> List.filter (\word -> String.endsWith word input)
+            |> List.head
+            |> Maybe.withDefault
+                (getLastDigitWord (String.left (String.length input - 1) input))
+
+
+digitDictionary : Dict String Int
+digitDictionary =
+    Dict.fromList
+        [ ( "one", 1 )
+        , ( "two", 2 )
+        , ( "three", 3 )
+        , ( "four", 4 )
+        , ( "five", 5 )
+        , ( "six", 6 )
+        , ( "seven", 7 )
+        , ( "eight", 8 )
+        , ( "nine", 9 )
+        , ( "1", 1 )
+        , ( "2", 2 )
+        , ( "3", 3 )
+        , ( "4", 4 )
+        , ( "5", 5 )
+        , ( "6", 6 )
+        , ( "7", 7 )
+        , ( "8", 8 )
+        , ( "9", 9 )
+        ]
+
+
+digitWords : List String
+digitWords =
+    Dict.keys digitDictionary
---
+++ Elm/tests/Day01Tests.elm
@@ -0,0 +1,29 @@
+module Day01Tests exposing (part2, tests)
+
+import Day01
+import Day01Input exposing (example1, example2, input)
+import Expect
+import Test exposing (Test, describe, test)
+
+
+tests : Test
+tests =
+    describe "Part 1"
+        [ test "toCalibrationValue 38" <| always <| Expect.equal (Just 38) <| Day01.toCalibrationValue "pqr3stu8vwx"
+        , test "toCalibrationValue 77" <| always <| Expect.equal (Just 77) <| Day01.toCalibrationValue "treb7uchet"
+        , test "Example" <| always <| Expect.equal 142 <| Day01.part1 example1
+        , test "Puzzle" <| always <| Expect.equal 54573 <| Day01.part1 input
+        ]
+
+
+part2 : Test
+part2 =
+    describe "Part 2"
+        [ test "getFirstDigitWord two1nine" <| always <| Expect.equal "two" <| Day01.getFirstDigitWord "two1nine"
+        , test "getFirstDigitWord abcone2threexyz" <| always <| Expect.equal "one" <| Day01.getFirstDigitWord "abcone2threexyz"
+        , test "getLastDigitWord two1nine" <| always <| Expect.equal "nine" <| Day01.getLastDigitWord "two1nine"
+        , test "getLastDigitWord 7pqrstsixteen" <| always <| Expect.equal "six" <| Day01.getLastDigitWord "7pqrstsixteen"
+        , test "toCalibrationValueIncludingWords" <| always <| Expect.equal (Just 76) <| Day01.toCalibrationValueIncludingWords "7pqrstsixteen"
+        , test "Example" <| always <| Expect.equal 281 <| Day01.part2 example2
+        , test "Puzzle" <| always <| Expect.equal 54591 <| Day01.part2 input
+        ]
