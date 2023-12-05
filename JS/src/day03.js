d3c4fad1374a7757fb05dcedb899ca2891bb7098
DnMcky <dmackay@findmypast.com>
2023-12-05 12:06:50 +0000

    WIP working on day 3

--- JS/src/day03.js
+++ JS/src/day03.js
@@ -2,6 +2,45 @@ module.exports.part1 = (input) => {
   return 0;
 };
 
+module.exports.getSymbolCoordinates = (schematic) => {
+  const results = new Set();
+  
+  schematic.forEach((row, rowIndex) => { 
+    const matches = [...row.matchAll(/[^\d\.]/g)]
+    matches.forEach((match) => {
+      results.add(match.index + rowIndex * 1000)
+    })
+  });
+
+  return results;
+}
+
+module.exports.getPartNumbers = (schematic) => {
+  const partInformation = [];
+  schematic.forEach((row, rowIndex) => {
+    const matches = [...row.matchAll(/\d+/g)]
+    matches.forEach((match) => {
+      partInformation.push({partNumber: +match[0], coordinate: match.index + rowIndex * 1000, length: match[0].length})
+    })
+  })
+  return partInformation;
+} 
+
+module.exports.getSurroundingCoordinates = (partInformation) => {
+  const surroundingCoordinate = []
+
+  const {coordinate, length} = partInformation;
+
+  const leftOf = coordinate -1; // no need to worry about edges because of genius system 
+  const rightOf = coordinate + length;
+  for(i=leftOf;i<=rightOf;i++) {
+    surroundingCoordinate.push(i-1000, i+1000)
+  }
+  surroundingCoordinate.push(leftOf, rightOf)
+
+  return surroundingCoordinate;
+} 
+
 module.exports.part2 = (input) => {
   return 0;
 };
--- JS/src/day03.test.js
+++ JS/src/day03.test.js
@@ -1,7 +1,34 @@
-const { test } = require("@jest/globals");
-const { part1, part2 } = require("./day03");
+const { test, describe, expect } = require("@jest/globals");
+const { part1, part2, getSymbolCoordinates, getPartNumbers, getSurroundingCoordinates } = require("./day03");
 const Input = require("./input");
-const EXAMPLE_INPUT = [];
+const EXAMPLE_INPUT = [
+  '467..114..',
+  '...*......',
+  '..35..633.',
+  '......#...',
+  '617*......',
+  '.....+.58.',
+  '..592.....',
+  '......755.',
+  '...$.*....',
+  '.664.598..'
+];
+
+test('getSymbolCoordinates', () => {
+  expect(getSymbolCoordinates(EXAMPLE_INPUT)).toEqual(new Set([1003, 3006, 4003, 5005, 8003, 8005]))
+})
+
+test('getPartNumbers', () => {
+  const sut = getPartNumbers(EXAMPLE_INPUT);
+  expect(sut[0]).toEqual({partNumber: 467, coordinate: 0, length: 3 });
+  expect(sut[2]).toEqual({partNumber: 35, coordinate: 2002, length: 2 })
+  expect(sut[9]).toEqual({partNumber: 598, coordinate: 9005, length: 3})
+})
+
+test('getSurroundingCoordinates', () => {
+  const sut = getSurroundingCoordinates({partNumber: 35, coordinate: 2002, length: 2 });
+  expect(sut).toContain([1001,1002,1003,1004,2001,2004,3001,3002,3003,3004]);
+})
 
 test("Part 1 Example", () => {
   expect(part1(EXAMPLE_INPUT)).toBe(4361);
