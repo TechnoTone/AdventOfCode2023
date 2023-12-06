2aa65049a9d439ad5b6aad376ea0a1f581d0674b
Tony Hunt <thunt@findmypast.com>
2023-12-06 13:38:57 +0000

    Elm Day 2

--- Elm/src/Day02.elm
+++ Elm/src/Day02.elm
@@ -3,12 +3,40 @@ module Day02 exposing (..)
 
 part1 : String -> Int
 part1 input =
-    0
+    let
+        validGame : Game -> Bool
+        validGame game =
+            game.rounds
+                |> List.all
+                    (\round ->
+                        round.red <= 12 && round.green <= 13 && round.blue <= 14
+                    )
+    in
+    input
+        |> String.lines
+        |> List.filterMap toGame
+        |> List.filter validGame
+        |> List.map .id
+        |> List.sum
 
 
 part2 : String -> Int
 part2 input =
-    0
+    input
+        |> String.lines
+        |> List.filterMap toGame
+        |> List.map toGamePower
+        |> List.sum
+
+
+type alias Game =
+    { id : Int
+    , rounds : List Round
+    }
+
+
+type alias Round =
+    { red : Int, green : Int, blue : Int }
 
 
 toGame : String -> Maybe Game
@@ -32,20 +60,56 @@ toRounds input =
     --3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     input
         |> String.split "; "
-        |> List.filterMap toRound
+        |> List.map toRound
 
 
-type alias Game =
-    { id : Int
-    , rounds : List Round
-    }
+toRound : String -> Round
+toRound input =
+    --3 blue, 4 red
+    input
+        |> String.split ", "
+        |> List.foldl
+            (\numberAndCube round ->
+                case String.split " " numberAndCube of
+                    [ numberStr, cube ] ->
+                        String.toInt numberStr
+                            |> Maybe.map
+                                (\number ->
+                                    case cube of
+                                        "red" ->
+                                            { round | red = number }
+
+                                        "green" ->
+                                            { round | green = number }
+
+                                        "blue" ->
+                                            { round | blue = number }
+
+                                        _ ->
+                                            round
+                                )
+                            |> Maybe.withDefault round
+
+                    _ ->
+                        round
+            )
+            (Round 0 0 0)
 
 
-type alias Round =
-    List CubeNumberAndColor
+toGamePower : Game -> Int
+toGamePower game =
+    game.rounds
+        |> List.foldl
+            (\round acc ->
+                Round
+                    (max round.red acc.red)
+                    (max round.green acc.green)
+                    (max round.blue acc.blue)
+            )
+            (Round 0 0 0)
+        |> toRoundPower
 
 
-type alias CubeNumberAndColor =
-    { number : Int
-    , color : String
-    }
+toRoundPower : Round -> Int
+toRoundPower round =
+    round.red * round.green * round.blue
--- Elm/tests/Day02Tests.elm
+++ Elm/tests/Day02Tests.elm
@@ -6,19 +6,37 @@ import Expect
 import Test exposing (Test, describe, test)
 
 
-part1 : Test
-part1 =
-    describe "Part 1"
-        [ test "Example" <| always <| Expect.equal 8 <| Day02.part1 example
-
-        -- , test "Puzzle" <| always <| Expect.equal 2505 <| Day02.part1 input
+parsing : Test
+parsing =
+    let
+        expectedParsedGame =
+            Just <|
+                Day02.Game 1
+                    [ Day02.Round 4 0 3
+                    , Day02.Round 1 2 6
+                    , Day02.Round 0 2 0
+                    ]
+    in
+    describe "Parsing"
+        [ test "x" <|
+            always <|
+                Expect.equal expectedParsedGame <|
+                    Day02.toGame "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
         ]
 
 
+part1 : Test
+part1 =
+    describe
+        "Part 1"
+        [ test "Example" <| always <| Expect.equal 8 <| Day02.part1 example
+        , test "Puzzle" <| always <| Expect.equal 2505 <| Day02.part1 input
+        ]
 
--- part2 : Test
--- part2 =
---     describe "Part 2"
---         [-- test "Example" <| always <| Expect.equal 2286 <| Day02.part2 example
---          -- , test "Puzzle" <| always <| Expect.equal 70265 <| Day02.part2 input
---         ]
+
+part2 : Test
+part2 =
+    describe "Part 2"
+        [ test "Example" <| always <| Expect.equal 2286 <| Day02.part2 example
+        , test "Puzzle" <| always <| Expect.equal 70265 <| Day02.part2 input
+        ]
