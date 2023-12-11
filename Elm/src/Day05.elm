9032ce78895b1877ae6a7ec35926f3280968d0fd
Tony Hunt <thunt@findmypast.com>
2023-12-11 14:57:00 +0000

    Elm Day 5 part 1 refactor

--- Elm/src/Day05.elm
+++ Elm/src/Day05.elm
@@ -3,8 +3,22 @@ module Day05 exposing (..)
 
 part1 : String -> Int
 part1 input =
+    let
+        applyMappings : List RangeTransformation -> List Int -> List Int
+        applyMappings mappings seeds =
+            seeds
+                |> List.map (mapSeed mappings)
+
+        mapSeed : List RangeTransformation -> Int -> Int
+        mapSeed rangeTransformations seed =
+            rangeTransformations
+                |> List.filter (seedInRange seed)
+                |> List.head
+                |> Maybe.map (.transformation >> (+) seed)
+                |> Maybe.withDefault seed
+    in
     getMaps input
-        |> List.foldl applyMappings (getSeeds input)
+        |> List.foldl applyMappings (getSimpleSeeds input)
         |> List.minimum
         |> Maybe.withDefault 0
 
@@ -14,30 +28,20 @@ part2 input =
     0
 
 
-applyMappings : List Mapping -> List Int -> List Int
-applyMappings mappings seeds =
-    seeds
-        |> List.map (mapSeed mappings)
-
-
-mapSeed : List Mapping -> Int -> Int
-mapSeed mappings seed =
-    mappings
-        |> List.filter (\mapping -> mapping.start <= seed && seed < mapping.start + mapping.length)
-        |> List.head
-        |> Maybe.withDefault (Mapping 0 0 0)
-        |> (\mapping -> seed + mapping.adjustment)
-
-
-type alias Mapping =
+type alias RangeTransformation =
     { start : Int
     , length : Int
-    , adjustment : Int
+    , transformation : Int
     }
 
 
-getSeeds : String -> List Int
-getSeeds input =
+seedInRange : Int -> RangeTransformation -> Bool
+seedInRange seed tx =
+    tx.start <= seed && seed < tx.start + tx.length
+
+
+getSimpleSeeds : String -> List Int
+getSimpleSeeds input =
     String.lines input
         |> List.head
         |> Maybe.withDefault ""
@@ -45,7 +49,7 @@ getSeeds input =
         |> List.filterMap String.toInt
 
 
-getMaps : String -> List (List Mapping)
+getMaps : String -> List (List RangeTransformation)
 getMaps input =
     case String.split "\n\n" input of
         _ :: sections ->
@@ -56,7 +60,7 @@ getMaps input =
             []
 
 
-getMap : String -> List Mapping
+getMap : String -> List RangeTransformation
 getMap input =
     case String.lines input of
         _ :: rows ->
@@ -67,11 +71,11 @@ getMap input =
             []
 
 
-getMapping : String -> Maybe Mapping
+getMapping : String -> Maybe RangeTransformation
 getMapping input =
     case input |> String.split " " |> List.filterMap String.toInt of
         [ destination, source, length ] ->
-            Just <| Mapping source length (destination - source)
+            Just <| RangeTransformation source length (destination - source)
 
         _ ->
             Nothing
