61290c840e9b7d563be1cd11bd879a6da3d2078b
Tony Hunt <thunt@findmypast.com>
2023-12-13 09:02:27 +0000

    Elm Day 5 part 2 WIP

--- Elm/src/Day05.elm
+++ Elm/src/Day05.elm
@@ -13,16 +13,16 @@ part1 input =
                 |> String.split " "
                 |> List.filterMap String.toInt
 
-        seedInRange : Int -> RangeTransformation -> Bool
+        seedInRange : Int -> MapTransformation -> Bool
         seedInRange seed tx =
-            tx.start <= seed && seed < tx.start + tx.length
+            tx.start <= seed && seed <= tx.end
 
-        applyMappings : List RangeTransformation -> List Int -> List Int
-        applyMappings mappings seeds =
+        applyMappings1 : List MapTransformation -> List Int -> List Int
+        applyMappings1 mappings seeds =
             seeds
                 |> List.map (mapSeed mappings)
 
-        mapSeed : List RangeTransformation -> Int -> Int
+        mapSeed : List MapTransformation -> Int -> Int
         mapSeed rangeTransformations seed =
             rangeTransformations
                 |> List.filter (seedInRange seed)
@@ -31,24 +31,134 @@ part1 input =
                 |> Maybe.withDefault seed
     in
     getMaps input
-        |> List.foldl applyMappings startSeeds
+        |> List.foldl applyMappings1 startSeeds
         |> List.minimum
         |> Maybe.withDefault 0
 
 
-part2 : String -> Int
+
+-- part2 : String -> Int
+
+
 part2 input =
-    0
+    let
+        startSeedRanges : List SeedRange
+        startSeedRanges =
+            input
+                |> String.lines
+                |> List.head
+                |> Maybe.withDefault ""
+                |> String.split " "
+                |> List.filterMap String.toInt
+                |> pair
+
+        pair : List Int -> List SeedRange
+        pair ints =
+            case ints of
+                start :: length :: rest ->
+                    SeedRange start (start + length - 1) :: pair rest
+
+                _ ->
+                    []
+    in
+    startSeedRanges
+        |> List.concatMap (applyMappings <| getMaps input)
 
 
-type alias RangeTransformation =
+applyMappings : List (List MapTransformation) -> SeedRange -> List SeedRange
+applyMappings mappings seedRange =
+    mappings
+        |> List.concatMap (applyMapping seedRange)
+
+
+applyMapping : SeedRange -> List MapTransformation -> List SeedRange
+applyMapping seedRange mapping =
+    mapping
+        |> List.concatMap (applyMap seedRange)
+        |> List.map (applyTransformations mapping)
+
+
+applyMap : SeedRange -> MapTransformation -> List SeedRange
+applyMap seedRange map =
+    let
+        overlap =
+            getOverlapType seedRange map
+
+        _ =
+            Debug.log "overlap" ( seedRange, map, overlap )
+    in
+    case getOverlapType seedRange map of
+        None ->
+            [ seedRange ]
+
+        Outer ->
+            [ SeedRange seedRange.start (map.start - 1), SeedRange map.start map.end, SeedRange (map.end + 1) seedRange.end ]
+
+        Left ->
+            [ SeedRange seedRange.start (map.start - 1), SeedRange map.start seedRange.end ]
+
+        Right ->
+            [ SeedRange seedRange.start map.end, SeedRange (map.end + 1) seedRange.end ]
+
+        Inner ->
+            [ seedRange ]
+
+
+applyTransformations : List MapTransformation -> SeedRange -> SeedRange
+applyTransformations mappings seedRange =
+    mappings
+        |> List.filter (\tx -> tx.start <= seedRange.start && tx.end >= seedRange.end)
+        |> List.head
+        |> Maybe.map (\tx -> SeedRange (seedRange.start + tx.transformation) (seedRange.end + tx.transformation))
+        |> Maybe.withDefault seedRange
+
+
+getOverlapType : SeedRange -> MapTransformation -> OverlapType
+getOverlapType seedRange mapping =
+    if seedRange.end < mapping.start then
+        None
+
+    else if mapping.end < seedRange.start then
+        None
+
+    else if seedRange.start >= mapping.start && seedRange.end <= mapping.end then
+        Inner
+
+    else if seedRange.start < mapping.start && seedRange.end > mapping.end then
+        Outer
+
+    else if seedRange.start < mapping.start && seedRange.end >= mapping.start then
+        Left
+
+    else if seedRange.start <= mapping.end && seedRange.end > mapping.end then
+        Right
+
+    else
+        None
+
+
+type alias MapTransformation =
     { start : Int
-    , length : Int
+    , end : Int
     , transformation : Int
     }
 
 
-getMaps : String -> List (List RangeTransformation)
+type alias SeedRange =
+    { start : Int
+    , end : Int
+    }
+
+
+type OverlapType
+    = None
+    | Outer
+    | Left
+    | Right
+    | Inner
+
+
+getMaps : String -> List (List MapTransformation)
 getMaps input =
     case String.split "\n\n" input of
         _ :: sections ->
@@ -59,7 +169,7 @@ getMaps input =
             []
 
 
-getMap : String -> List RangeTransformation
+getMap : String -> List MapTransformation
 getMap input =
     case String.lines input of
         _ :: rows ->
@@ -70,11 +180,11 @@ getMap input =
             []
 
 
-getMapping : String -> Maybe RangeTransformation
+getMapping : String -> Maybe MapTransformation
 getMapping input =
     case input |> String.split " " |> List.filterMap String.toInt of
         [ destination, source, length ] ->
-            Just <| RangeTransformation source length (destination - source)
+            Just <| MapTransformation source (source + length - 1) (destination - source)
 
         _ ->
             Nothing
--- Elm/tests/Day05Tests.elm
+++ Elm/tests/Day05Tests.elm
@@ -1,6 +1,6 @@
 module Day05Tests exposing (..)
 
-import Day05
+import Day05 exposing (MapTransformation, SeedRange)
 import Day05Input exposing (..)
 import Expect
 import Test exposing (Test, describe, test)
@@ -14,10 +14,23 @@ part1 =
         ]
 
 
+mapSeedRangeTests : Test
+mapSeedRangeTests =
+    describe "mapSeedRange"
+        [ --test "No overlap (left)" <| always <| Expect.equal [ SeedRange 10 20 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 21 4 0 ]
+          -- , test "No overlap (right)" <| always <| Expect.equal [ SeedRange 10 20 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 5 9 0 ]
+          -- , test "Part overlap (left)" <| always <| Expect.equal [ SeedRange 110 112, SeedRange 13 20 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 8 12 100 ]
+          -- , test "Part overlap (right)" <| always <| Expect.equal [ SeedRange 10 17, SeedRange 118 120 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 18 22 100 ]
+          -- , test "Contained within" <| always <| Expect.equal [ SeedRange 110 120 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 1 30 100 ]
+          -- , test "Overlaps both sides" <| always <| Expect.equal [ SeedRange 10 12, SeedRange 113 117, SeedRange 18 20 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 13 17 100 ]
+          test "Doesn't chain-react with other map ranges" <| always <| Expect.equal [ SeedRange 30 40 ] <| Day05.applyMapping (SeedRange 10 20) [ MapTransformation 10 20 20, MapTransformation 30 40 20 ]
+        ]
+
+
 
 -- part2 : Test
 -- part2 =
 --     describe "Part 2"
---         [-- test "Example" <| always <| Expect.equal 0 <| Day05.part2 example
+--         [-- test "Example" <| always <| Expect.equal "46" <| Debug.toString <| Day05.part2 example
 --          -- , test "Puzzle" <| always <| Expect.equal 0 <| Day05.part2 input
 --         ]
