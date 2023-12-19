ba75b21a63e659a9d6a52a17a729254bec8c3718
Tony Hunt <thunt@findmypast.com>
2023-12-19 15:47:58 +0000

    Day 6 prep

---
+++ JS/src/day06.test.js
@@ -0,0 +1,22 @@
+const { test } = require("@jest/globals");
+const { part1, part2 } = require("./day06");
+const Input = require("./input");
+const EXAMPLE_INPUT = ["Time:      7  15   30", "Distance:  9  40  200"];
+
+test("Part 1 Example", () => {
+  expect(part1(EXAMPLE_INPUT)).toBe(288);
+});
+
+test.skip("Part 1", () => {
+  const input = new Input(6).fromLines().get();
+  expect(part1(input)).toBe(0);
+});
+
+test.skip("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT)).toBe(0);
+});
+
+test.skip("Part 2", () => {
+  const input = new Input(6).fromLines().get();
+  expect(part2(input)).toBe(0);
+});
