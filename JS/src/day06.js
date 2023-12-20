d7fe725315a5618ac17726d83709c8ac965d4c7e
Tony Hunt <thunt@findmypast.com>
2023-12-20 10:00:14 +0000

    Day 6 complete

--- JS/src/day06.js
+++ JS/src/day06.js
@@ -1,7 +1,27 @@
 module.exports.part1 = (input) => {
-  return 0;
+  const times = input[0].match(/\d+/g).map(Number);
+  const distances = input[1].match(/\d+/g).map(Number);
+
+  return times.reduce((acc, t, i) => acc * possibleWins(t, distances[i]), 1);
+  // ( -b - sqrt( b^2 - 4ac ) ) / 2a
+  // ( -b + sqrt( b^2 - 4ac ) ) / 2a
 };
 
+function possibleWins(t, d) {
+  const a = 1;
+  const b = -t;
+  const c = d;
+  const root = Math.sqrt(b * b - 4 * a * c);
+  const x1 = (-b - root) / (2 * a);
+  const x2 = (-b + root) / (2 * a);
+  const lowest = Math.trunc(x1) === x1 ? x1 + 1 : Math.ceil(x1);
+  const highest = Math.trunc(x2) === x2 ? x2 - 1 : Math.floor(x2);
+  return highest - lowest + 1;
+}
+
 module.exports.part2 = (input) => {
-  return 0;
+  const time = Number(input[0].split(":")[1].replaceAll(" ", ""));
+  const distance = Number(input[1].split(":")[1].replaceAll(" ", ""));
+
+  return possibleWins(time, distance);
 };
--- JS/src/day06.test.js
+++ JS/src/day06.test.js
@@ -7,16 +7,16 @@ test("Part 1 Example", () => {
   expect(part1(EXAMPLE_INPUT)).toBe(288);
 });
 
-test.skip("Part 1", () => {
+test("Part 1", () => {
   const input = new Input(6).fromLines().get();
-  expect(part1(input)).toBe(0);
+  expect(part1(input)).toBe(160816);
 });
 
-test.skip("Part 2 Example", () => {
-  expect(part2(EXAMPLE_INPUT)).toBe(0);
+test("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT)).toBe(71503);
 });
 
-test.skip("Part 2", () => {
+test("Part 2", () => {
   const input = new Input(6).fromLines().get();
-  expect(part2(input)).toBe(0);
+  expect(part2(input)).toBe(46561107);
 });
