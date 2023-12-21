992b88a2b357249a3f7e88a220914df97ea332a1
Tony Hunt <thunt@findmypast.com>
2023-12-21 14:25:26 +0000

    Elm Day 6

--- Elm/src/Day06.elm
+++ Elm/src/Day06.elm
@@ -3,36 +3,50 @@ module Day06 exposing (..)
 import List.Extra as List
 
 
-
--- part1 : String -> Int
-
-
+part1 : String -> Int
 part1 input =
+    let
+        parseLine : String -> List Int
+        parseLine line =
+            line
+                |> String.split " "
+                |> List.filterMap String.toInt
+    in
     input
-        |> parseInput
+        |> parseInput parseLine
         |> List.map countWins
+        |> List.product
 
 
 part2 : String -> Int
 part2 input =
-    0
+    let
+        parseLine : String -> List Int
+        parseLine line =
+            line
+                |> String.split ":"
+                |> List.reverse
+                |> List.head
+                |> Maybe.map (String.replace " " "")
+                |> Maybe.andThen String.toInt
+                |> Maybe.withDefault 0
+                |> List.singleton
+    in
+    input
+        |> parseInput parseLine
+        |> List.head
+        |> Maybe.map countWins
+        |> Maybe.withDefault 0
 
 
-parseInput : String -> List ( Int, Int )
-parseInput input =
+parseInput : (String -> List Int) -> String -> List ( Int, Int )
+parseInput lineParser input =
     case String.lines input of
         [ a, b ] ->
-            List.zip (parseLine a) (parseLine b)
+            List.zip (lineParser a) (lineParser b)
 
         _ ->
             []
-
-
-parseLine : String -> List Int
-parseLine line =
-    line
-        |> String.split " "
-        |> List.filterMap String.toInt
 
 
 countWins : ( Int, Int ) -> Int
@@ -51,19 +65,23 @@ countWins ( t, d ) =
             b * b - 4 * a * c
 
         x1 =
-            Debug.log "x1" <|
-                (-b - sqrt discriminant)
-                    / (2 * a)
+            (-b - sqrt discriminant) / (2 * a)
 
         x2 =
-            Debug.log "x2" <|
-                (-b + sqrt discriminant)
-                    / (2 * a)
+            (-b + sqrt discriminant) / (2 * a)
 
         lower =
-            ceiling x1
+            if (round x1 |> toFloat) == x1 then
+                ceiling x1 + 1
+
+            else
+                ceiling x1
 
         upper =
-            floor x2
+            if (round x2 |> toFloat) == x2 then
+                floor x2 - 1
+
+            else
+                floor x2
     in
     upper - lower + 1
