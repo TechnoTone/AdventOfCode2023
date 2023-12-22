67e5232948a2d4c77076d658a984ed347ebdae2e
DnMcky <dmackay@findmypast.com>
2023-12-22 10:01:58 +0000

    day 8 wip

--- JS/src/day08.js
+++ JS/src/day08.js
@@ -1,7 +1,57 @@
 module.exports.part1 = (input) => {
-  return 0;
+  const [directions, _, ...nodes] = input;
+
+  const network = nodes.reduce((acc, node) => {
+    const location = node.slice(0, 3)
+    const destinationLeft = node.slice(7, 10)
+    const destinationRight = node.slice(12, 15)
+
+    return acc.set(location, [destinationLeft, destinationRight])
+  }, new Map());
+
+  let currentLocation = "AAA";
+  let steps = 0;
+  while (currentLocation !== "ZZZ") {
+    const destinationIndex = directions[steps % directions.length] === "L" ? 0 : 1;
+    currentLocation = network.get(currentLocation)[destinationIndex]
+    steps++;
+  }
+
+  return steps;
 };
 
 module.exports.part2 = (input) => {
-  return 0;
+  const [directions, _, ...nodes] = input;
+
+  const [network, starts] = nodes.reduce(([network, starts], node) => {
+    const location = node.slice(0, 3)
+    const destinationLeft = node.slice(7, 10)
+    const destinationRight = node.slice(12, 15)
+
+    if (location[2] === "A") {
+      starts.add(location)
+    }
+
+    return [network.set(location, [destinationLeft, destinationRight]), starts]
+  }, [new Map(), new Set()]);
+
+
+  function traverseNetwork(currentLocation) {
+    let steps = 0
+    while (currentLocation[2] !== "Z") {
+      const destinationIndex = directions[steps % directions.length] === "L" ? 0 : 1;
+      currentLocation = network.get(currentLocation)[destinationIndex]
+      steps++;
+    }
+    return steps;
+  }
+
+  starts.forEach((startLocation) => {
+
+    console.log(startLocation, traverseNetwork(startLocation))
+  })
+
+  return [network, starts];
 };
+
+
--- JS/src/day08.test.js
+++ JS/src/day08.test.js
@@ -20,6 +20,19 @@ const EXAMPLE_INPUT_2 = [
   "ZZZ = (ZZZ, ZZZ)",
 ];
 
+const EXAMPLE_INPUT_3 = [
+  "LR",
+  "",
+  "11A = (11B, XXX)",
+  "11B = (XXX, 11Z)",
+  "11Z = (11B, XXX)",
+  "22A = (22B, XXX)",
+  "22B = (22C, 22C)",
+  "22C = (22Z, 22Z)",
+  "22Z = (22B, 22B)",
+  "XXX = (XXX, XXX)",
+]
+
 test("Part 1 Example 1", () => {
   expect(part1(EXAMPLE_INPUT_1)).toBe(2);
 });
@@ -28,13 +41,18 @@ test("Part 1 Example 2", () => {
   expect(part1(EXAMPLE_INPUT_2)).toBe(6);
 });
 
-test.skip("Part 1", () => {
+test("Part 1", () => {
   const input = new Input(8).fromLines().get();
-  expect(part1(input)).toBe(0);
+  expect(part1(input)).toBe(17287);
 });
 
-test.skip("Part 2 Example", () => {
-  expect(part2(EXAMPLE_INPUT_1)).toBe(0);
+test("Part 1 dan", () => {
+  const input = new Input(8, true).fromLines().get();
+  expect(part1(input)).toBe(20221);
+});
+
+test("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT_3)).toBe(0);
 });
 
 test.skip("Part 2", () => {
