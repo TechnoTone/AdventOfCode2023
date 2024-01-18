5674c1f99f0ce65af4ba4ce43abc139d741b1a0b
DnMcky <dmackay@findmypast.com>
2024-01-18 09:55:39 +0000

    solve day 11

--- JS/src/day11.js
+++ JS/src/day11.js
@@ -1,7 +1,85 @@
 module.exports.part1 = (input) => {
-  return 0;
+  // find and map galaxies coordinates
+  const { galaxyCoordinates, rowExpansions } = input.reduce((acc, row, yIndex) => {
+    const coordinates = [...row.matchAll(/#/g)].map((match) => [match.index, yIndex])
+    if (coordinates.length === 0) {
+      acc.rowExpansions.push(yIndex)
+    } else {
+      acc.galaxyCoordinates.push(...coordinates)
+    }
+    return acc
+  }, {
+    galaxyCoordinates: [],
+    rowExpansions: []
+  });
+
+  // find columns to expand
+  const columnExpansions = input[0].split("").reduce((acc, _, columnIndex) => {
+    if (galaxyCoordinates.every(([x]) => x !== columnIndex)) {
+      acc.push(columnIndex)
+    }
+    return acc
+  }, []);
+
+  const pairedGalaxies = getAllPairs(galaxyCoordinates);
+
+  const galaxyDistances = pairedGalaxies.map((pair) => {
+    return getExpansion(pair, rowExpansions, columnExpansions) + calculateDistance(pair)
+  })
+
+  return galaxyDistances.reduce((acc, distance) => acc + distance);
+};
+
+function getExpansion([[ax, ay], [bx, by]], rowExpansions, columnExpansions, factor = 2) {
+  return columnExpansions.filter((column) => inRange(column, [ax, bx])).length * (factor - 1)
+    + rowExpansions.filter((row) => inRange(row, [ay, by])).length * (factor - 1);
+};
+
+function inRange(expansion, [a, b]) {
+  return (expansion > Math.min(a, b) && expansion < Math.max(a, b))
+}
+
+function calculateDistance([[ax, ay], [bx, by]]) {
+  return Math.abs(ax - bx) + Math.abs(ay - by);
+};
+
+function getAllPairs(list) {
+  return list.reduce((acc, item, index) => {
+    for (let i = index + 1; i < list.length; i++) {
+      acc.push([item, list[i]]);
+    }
+    return acc
+  }, []);
 };
 
 module.exports.part2 = (input) => {
-  return 0;
+  // find and map galaxies coordinates
+  const { galaxyCoordinates, rowExpansions } = input.reduce((acc, row, yIndex) => {
+    const coordinates = [...row.matchAll(/#/g)].map((match) => [match.index, yIndex])
+    if (coordinates.length === 0) {
+      acc.rowExpansions.push(yIndex)
+    } else {
+      acc.galaxyCoordinates.push(...coordinates)
+    }
+    return acc
+  }, {
+    galaxyCoordinates: [],
+    rowExpansions: []
+  });
+
+  // find columns to expand
+  const columnExpansions = input[0].split("").reduce((acc, _, columnIndex) => {
+    if (galaxyCoordinates.every(([x]) => x !== columnIndex)) {
+      acc.push(columnIndex)
+    }
+    return acc
+  }, []);
+
+  const pairedGalaxies = getAllPairs(galaxyCoordinates);
+
+  const galaxyDistances = pairedGalaxies.map((pair) => {
+    return getExpansion(pair, rowExpansions, columnExpansions, 1000000) + calculateDistance(pair)
+  })
+
+  return galaxyDistances.reduce((acc, distance) => acc + distance);
 };
--- JS/src/day11.test.js
+++ JS/src/day11.test.js
@@ -18,16 +18,26 @@ test("Part 1 Example", () => {
   expect(part1(EXAMPLE_INPUT)).toBe(374);
 });
 
-test.skip("Part 1", () => {
+test("Part 1", () => {
   const input = new Input(11).fromLines().get();
-  expect(part1(input)).toBe(0);
+  expect(part1(input)).toBe(9609130);
 });
 
-test.skip("Part 2 Example", () => {
-  expect(part2(EXAMPLE_INPUT)).toBe(0);
+test("Part 1 Dan", () => {
+  const input = new Input(11, true).fromLines().get();
+  expect(part1(input)).toBe(10276166);
 });
 
-test.skip("Part 2", () => {
+test("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT)).toBe(82000210);
+});
+
+test("Part 2", () => {
   const input = new Input(11).fromLines().get();
-  expect(part2(input)).toBe(0);
+  expect(part2(input)).toBe(702152204842);
+});
+
+test("Part 2 Dan", () => {
+  const input = new Input(11, true).fromLines().get();
+  expect(part2(input)).toBe(598693078798);
 });
