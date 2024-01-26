0f4006e29f0a8d0fb388dc709964e90b2ecb714b
Tony Hunt <thunt@findmypast.com>
2024-01-26 09:38:43 +0000

    JS Day 12

---
+++ JS/src/day12.js
@@ -0,0 +1,57 @@
+module.exports.part1 = (input) => {
+  const memo = new Map();
+  return input.reduce((acc, row) => count(parseRow(row), memo) + acc, 0);
+};
+
+module.exports.part2 = (input) => {
+  const memo = new Map();
+  return input.reduce((acc, row) => count(parseRow(row, true), memo) + acc, 0);
+};
+
+function parseRow(row, timesFive = false) {
+  const [springs, groups] = row.split(" ");
+  const numberGroups = groups.split(",").map(Number);
+
+  if (timesFive) {
+    return [
+      springs.padEnd(springs.length * 5 + 4, "?" + springs),
+      Array(5).fill(numberGroups).flat(),
+    ];
+  }
+  return [springs, numberGroups];
+}
+
+function count([springs, groups], memo) {
+  const key = springs + " " + groups.join(",");
+  if (memo.has(key)) {
+    return memo.get(key);
+  }
+
+  if (groups.length === 0) {
+    const result = springs.includes("#") ? 0 : 1;
+    memo.set(key, result);
+    return result;
+  }
+
+  if (springs.length < groups[0]) {
+    memo.set(key, 0);
+    return 0;
+  }
+
+  let total = 0;
+
+  if (springs[0] !== "#") {
+    total += count([springs.slice(1), groups], memo);
+  }
+
+  if (
+    !springs.slice(0, groups[0]).includes(".") &&
+    springs[groups[0]] !== "#"
+  ) {
+    total += count([springs.slice(groups[0] + 1), groups.slice(1)], memo);
+  }
+
+  memo.set(key, total);
+
+  return total;
+}
---
+++ JS/src/day12.test.js
@@ -0,0 +1,29 @@
+const { test } = require("@jest/globals");
+const { part1, part2 } = require("./day12");
+const Input = require("./input");
+const EXAMPLE_INPUT = [
+  "???.### 1,1,3",
+  ".??..??...?##. 1,1,3",
+  "?#?#?#?#?#?#?#? 1,3,1,6",
+  "????.#...#... 4,1,1",
+  "????.######..#####. 1,6,5",
+  "?###???????? 3,2,1",
+];
+
+test("Part 1 Example", () => {
+  expect(part1(EXAMPLE_INPUT)).toBe(21);
+});
+
+test("Part 1", () => {
+  const input = new Input(12).fromLines().get();
+  expect(part1(input)).toBe(7732);
+});
+
+test("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT)).toBe(525152);
+});
+
+test("Part 2", () => {
+  const input = new Input(12).fromLines().get();
+  expect(part2(input)).toBe(4500070301581);
+});
